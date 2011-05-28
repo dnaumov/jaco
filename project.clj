209f327c0b8c8dfd817efb6a1c8371b81535e616
Dmitri Naumov <ru.vinzent@gmail.com>
2011-05-28 17:46:16 +0600

    updated deps

--- project.clj
+++ project.clj
@@ -2,9 +2,8 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [ring/ring-jetty-adapter "0.3.1"]                                  
-                 [compojure "0.6.0-RC4"]
-                 [hiccup "0.3.4"]]
+                 [compojure "0.6.3"]
+                 [hiccup "0.3.5"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [leiningen-midje "1.0.0-SNAPSHOT"]
-                     [midje "1.1"]])
+                     [midje "1.1.1"]])

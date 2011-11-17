39bfd1d18e13476a2b7a5a85ee24b3abeef600b8
Dmitri Naumov <ru.vinzent@gmail.com>
2011-11-17 21:34:38 +0600

    updated deps; clojure 1.3

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject jaco "0.0.1-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.6.3"]
-                 [hiccup "0.3.5"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
-                     [leiningen-midje "1.0.0-SNAPSHOT"]
-                     [midje "1.1.1"]])
+  :description "A web framework built on top of Compojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.macro "0.1.1"]
+                 [compojure "0.6.5"]
+                 [hiccup "0.3.7"]]
+  :dev-dependencies [[midje "1.3-alpha5"]])
